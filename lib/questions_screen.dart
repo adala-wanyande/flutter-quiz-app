f244345e6d377f3a2f0c1d97c8f7991f968beebf
adala-wanyande <benardwanyande@gmail.com>
2024-01-27 19:01:02 +0300

    chore: access list elements and question object properties

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/answer_button.dart';
+// import 'package:quiz_app/models/quiz_question.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,27 +14,28 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Column(children: [
-            const Text(
-              'The question...',
-              style: TextStyle(color: Colors.white),
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
             ),
             const SizedBox(height: 30),
             AnswerButton(
-              answerText: 'Answer 1',
+              answerText: currentQuestion.answers[0],
               onTap: () {},
             ),
             AnswerButton(
-              answerText: 'Answer 2',
+              answerText: currentQuestion.answers[1],
               onTap: () {},
             ),
             AnswerButton(
-              answerText: 'Answer 3',
+              answerText: currentQuestion.answers[2],
               onTap: () {},
             ),
           ]),
